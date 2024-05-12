dd21bc2a3fd37bce5e757b3b9da2c445fb5fd021
Dream-Hunter <hunter@it-answer.ru>
2024-05-12 14:03:02 +0700

    update

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This is the basic method getting data from binance wss
 
 Enjoy!
+
+2024-05-12
+
+Added watchdog feature to allow reconnection after connection loss.
--- binance-wss.pl
+++ binance-wss.pl
@@ -3,44 +3,77 @@
 use strict;
 use IO::Async::Loop;
 use Net::Async::WebSocket::Client;
+use IO::Async::Timer::Periodic;
 use JSON;
 use POSIX;
 use Data::Dumper;
 
-my $client;
 my $host = "stream.binance.com";
 my $port = "443";
+my $heartbeat_interval = 300;
+my $heartbeat;
 #################################
 # Async Web Socket Client handler
 #################################
-$client = Net::Async::WebSocket::Client->new(
-    on_ping_frame => sub {
-        my ( $bytes ) = $_[0];
-        printf ("%s %s", strftime("%Y-%m-%d %H:%M:%S ",localtime), "Ping frame received - sending Pong");
-        $client->send_pong_frame($bytes);
-    },
-    on_text_frame => sub {
-        my $data = dataHandler(@_);
-    }
-);
+while (1) {
+    my $timer  = undef;
+    my $client = undef;
+    my $loop   = undef;
 
-my $loop = IO::Async::Loop->new;
+    $heartbeat = time+$heartbeat_interval;
 
-$loop->add( $client );
+    $timer = IO::Async::Timer::Periodic->new(
+        interval=> 10,
+        on_tick => sub {
+            if ($heartbeat < time) {
+                print strftime("%Y-%m-%d %H:%M:%S ", localtime);
+                print "Close connection...\n";
+                $client->close_now;
+                $timer->stop;
+                $loop->loop_stop;
+            } else {
+                print strftime("%Y-%m-%d %H:%M:%S ", localtime);
+                printf ("Heartbeat is fine %s > %s\n", $heartbeat, time) ;
+            }
+        }
+    );
 
-$client->connect(
-   host => $host,
-   service => $port,
-   url => "wss://$host:$port/stream",
-)->get;
+    $client = Net::Async::WebSocket::Client->new(
+        on_ping_frame => sub {
+            my ( $bytes ) = $_[0];
+            printf ("%s %s", strftime("%Y-%m-%d %H:%M:%S ",localtime), "Ping frame received - sending Pong\n");
+            $client->send_pong_frame($bytes);
+            $heartbeat = time+$heartbeat_interval;
+        },
+        on_text_frame => sub {
+            my $data = dataHandler(@_);
+        }
+    );
 
-print strftime("%Y-%m-%d %H:%M:%S ", localtime);
-print "Connected; go ahead...\n";
+    $loop = IO::Async::Loop->new;
 
-my $datasend = {"method" => "SUBSCRIBE","params" => ["btcusdt\@aggTrade"],"id" => 1};
-$client->send_text_frame( encode_json($datasend) );
+    $timer->start;
+    $loop->add( $timer );
 
-$loop->run;
+    $loop->add( $client );
+    $client->connect(
+       host => $host,
+       service => $port,
+       url => "wss://$host:$port/stream",
+    )->get;
+
+    print strftime("%Y-%m-%d %H:%M:%S ", localtime);
+    print "Connected; go ahead...\n";
+
+    my $datasend = {"method" => "SUBSCRIBE","params" => ["btcusdt\@aggTrade"],"id" => 1};
+    $client->send_text_frame( encode_json($datasend) );
+
+    $loop->run;
+
+    sleep 10;
+    print strftime("%Y-%m-%d %H:%M:%S ", localtime);
+    print "Start again\n";
+}
 #############
 # Subroutines
 #############
